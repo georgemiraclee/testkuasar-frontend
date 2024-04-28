da94c83a8dbecaad3f291eaf47a6d501fdb76e90
George Miracle <97882379+georgemiraclee@users.noreply.github.com>
2024-04-28 20:52:23 +0700

    first commit

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import '../styles/logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <h1>
+        List <span className="logo_secondary">Country</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Logo;
